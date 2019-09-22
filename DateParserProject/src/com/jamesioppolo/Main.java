eda03720cfeddc4443cea8235f37012a396d8606
James Ioppolo <james.ioppolo@gmail.com>
2019-09-23 09:31:30 +1000

    Update Main.java

--- DateParserProject/src/com/jamesioppolo/Main.java
+++ DateParserProject/src/com/jamesioppolo/Main.java
@@ -10,11 +10,11 @@ public static void main(String[] args) {
 
 	// Initialises Spring
 	Resource resource=new ClassPathResource("applicationContext.xml");  
-    BeanFactory factory=new XmlBeanFactory(resource);  
+    	BeanFactory factory=new XmlBeanFactory(resource);  
       
-    // Gets the date parser from the container and executes it
-    System.out.println("Date Parser Console Input");
-    ConsoleInputDateParser dateParser=(ConsoleInputDateParser)factory.getBean("consoleinputdateparser");  
-    dateParser.execute();      
-}  
+    	// Gets the date parser from the container and executes it
+    	System.out.println("Date Parser Console Input");
+    	ConsoleInputDateParser dateParser=(ConsoleInputDateParser)factory.getBean("consoleinputdateparser");  
+    	dateParser.execute();      
+	}  
 }  
